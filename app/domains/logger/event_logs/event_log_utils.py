0b0f1cb99ec130dcb6ad6c3cb3cb511106bb26e5
ho8566z <ho8566z@gmail.com>
2026-06-29 12:13:57 +0900

    event_log 수정

--- app/domains/logger/event_logs/event_log_utils.py
+++ app/domains/logger/event_logs/event_log_utils.py
@@ -1,12 +1,19 @@
 from flask import send_file
+from app.utils.json_manager import load_json, TARGETS_FILE
 import pandas as pd
 import io
 
 
 def format_events(events):
+    targets = load_json(TARGETS_FILE)
+
     cleaned_events = []
 
     for event in events:
+
+        target_id = event.get("TARGET_ID")
+        target = targets.get(target_id, {})
+
         place_text = f"{event.get('latitude')}, {event.get('longitude')}"
             
         cleaned_events.append(
@@ -14,7 +21,7 @@ def format_events(events):
                 "ID": event.get("ID"),
                 "시간": event.get("REG_DATE"),
                 "장소": place_text,
-                "대상": event.get("TARGET_ID"),
+                "대상ID": target_id
             }
         )
     return cleaned_events
--- app/domains/logger/event_logs/event_logs.py
+++ app/domains/logger/event_logs/event_logs.py
@@ -1,4 +1,5 @@
-from app.utils.json_manager import load_json
+from app.utils.json_manager import load_json, save_json
+from app.utils.time_stamper import get_current_time_stamp_formated
 from app.utils.json_manager import EVENT_LOGS_FILE
 
 def get_event_list():
@@ -6,6 +7,21 @@ def get_event_list():
     logs = load_json(EVENT_LOGS_FILE)
 
     return list(logs.values())
+
+
+def add_event(data):
+
+    logs = load_json(EVENT_LOGS_FILE)
+
+    logs[data["ID"]] = {
+        "ID": data["ID"],
+        "REG_DATE": get_current_time_stamp_formated(),
+        "latitude": data["latitude"],
+        "longitude": data["longitude"],
+        "TARGET_ID": data["TARGET_ID"]
+    }
+
+    save_json(EVENT_LOGS_FILE, logs)
 
 
 if __name__ == "__main__":
