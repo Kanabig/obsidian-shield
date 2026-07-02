e6e17a848a4cbce3551fe54023402862183735a2
giljungyu62 <giljungyu62>
2026-07-02 12:49:31 +0900

    commit info-window 추가by ktj 12:49

--- app/domains/map/static/js/map.js
+++ app/domains/map/static/js/map.js
@@ -1,22 +1,52 @@
+console.log(maps);
 // 1. 지도를 띄울 중심좌표와 확대 레벨
-const lat = 36.332326;
-const lng = 127.434211;
+const centerlat = maps[0].latitude;
+const centerlng = maps[0].longitude;
 
 
 var options = {
-        center: new kakao.maps.LatLng(lat, lng),
+        center: new kakao.maps.LatLng(centerlat, centerlng),
         level: 3
 };
 
 //2. HTML에서 지도를 담을 컨테이너(div)를 가져와 실제 지도 객체 생성
-var container = document.getElementById('map');
+var container = document.getElementById("map");
 var map = new kakao.maps.Map(container, options);
 
-//3. HTML에서 지도를 담을 컨테이너(div)를 가져와 실제 지도 객체 생성
-var markerpostion = new kakao.maps.LatLng(lat, lng);
 
-//4. 마커 객체 생성
-var marker = new kakao.maps.Marker({position: markerpostion});
+//3. 마커 객체 생성
+maps.forEach(function(target){
+        const marker = new kakao.maps.Marker({
+                map: map,
+                position: new kakao.maps.LatLng(
+                        target.latitude,
+                        target.longitude
+                )
+        });
 
-//5. 마커를 생성한 지도 위에 표시
-marker.setMap(map);
+
+        //4. Infowindow 내용
+        const content = `
+        
+        <div class = "info-window">
+            <img src="/static/images/${target.images}">
+            <h3>${target.name}</h3>
+            <p>나이: ${target.age}</p>
+            <p>${target.short_description}</p>
+            <p>탐지시간: ${target.reg_date}</p>
+        </div>
+        
+        `;
+        
+        //5. InfoWindow 생성
+        const infoWindow = new kakao.maps.InfoWindow({
+                content: content
+        });
+        //6. 마커 클릭시 열기
+        kakao.maps.event.addListener(marker, 'click', function() {
+                infoWindow.open(map, marker);
+        });
+});
+
+        
+
