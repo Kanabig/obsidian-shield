deff613d8e187ff40387608a9f405928aa587dc2
JDG <j75390@gmail>
2026-07-03 17:09:06 +0900

    key ID쪽으로 변경 configs이용

--- app/utils/member_filter.py
+++ app/utils/member_filter.py
@@ -1,3 +1,5 @@
+from app import configs
+
 def filter_keyword(accounts, keyword, tag):
 
     if not keyword:
@@ -6,19 +8,19 @@ def filter_keyword(accounts, keyword, tag):
     keyword = keyword.lower()
 
     if tag == "아이디":
-        return [a for a in accounts if keyword in a.get("ID", "").lower()]
+        return [a for a in accounts if keyword in a.get(configs.KEY_ID, "").lower()]
 
     elif tag == "이름":
-        return [a for a in accounts if keyword in a.get("NAME", "").lower()]
+        return [a for a in accounts if keyword in a.get(configs.KEY_NAME, "").lower()]
 
     elif tag == "역할":
-        return [a for a in accounts if keyword in ",".join(a.get("PERMISSIONS", [])).lower()]
+        return [a for a in accounts if keyword in ",".join(a.get(configs.KEY_PERMISSIONS, [])).lower()]
 
     return [
         a for a in accounts
-        if keyword in a.get("ID", "").lower()
-        or keyword in a.get("NAME", "").lower()
-        or keyword in ",".join(a.get("PERMISSIONS", [])).lower()
+        if keyword in a.get(configs.KEY_ID, "").lower()
+        or keyword in a.get(configs.KEY_NAME, "").lower()
+        or keyword in ",".join(a.get(configs.KEY_PERMISSIONS, [])).lower()
     ]
 
 
@@ -27,15 +29,15 @@ def filter_permission(accounts, permission):
     if permission == "전체":
         return accounts
 
-    return [a for a in accounts if permission in a.get("PERMISSIONS", [])]
+    return [a for a in accounts if permission in a.get(configs.KEY_PERMISSIONS, [])]
 
 
 def filter_approve(accounts, approve):
 
     if approve == "승인":
-        return [a for a in accounts if a.get("IS_APPROVE") is True]
+        return [a for a in accounts if a.get(configs.KEY_IS_APPROVE) is True]
 
     elif approve == "미승인":
-        return [a for a in accounts if a.get("IS_APPROVE") is False]
+        return [a for a in accounts if a.get(configs.KEY_IS_APPROVE) is False]
 
     return accounts
