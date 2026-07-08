1d42f13f9376df58a67d270da80cc9318141cae9
Kanabig <parkjungho20@gmail.com>
2026-07-08 12:07:10 +0900

    feat: profile registration

--- app/domains/stream/routes.py
+++ app/domains/stream/routes.py
@@ -1,4 +1,5 @@
 import time
+import os
 
 from flask import Blueprint, Response, render_template, request, redirect, url_for
 from cv2 import imencode
@@ -6,7 +7,12 @@ from cv2 import imencode
 from app.domains.stream import analysis_pipeline
 from app.domains.stream import camera as camera_manager
 from app.utils.pagination import paginate
-from app.utils.json_manager import load_json, TARGETS_PROFILES_FILE
+from app.utils.json_manager import load_json, save_json, TARGETS_PROFILES_FILE, BASE_DIR
+from app.utils.time_stamper import get_current_time_stamp_formated
+
+from app.utils.member_filter import filter_keyword
+from app.utils.member_sort import sort_accounts
+from werkzeug.utils import secure_filename
 
 stream_bp = Blueprint(
     "stream",
@@ -59,11 +65,60 @@ def camera():
 
 @stream_bp.route("/profile/", methods=["GET", "POST"])
 def profile():
+    # 프로필 CRUD 처리
+    if request.method == "POST":
+        profiles = load_json(TARGETS_PROFILES_FILE)
+
+        action = request.form.get("action")
+        if action == "add":
+            id = request.form.get("id")
+            name = request.form.get("name")
+            age = request.form.get("age")
+            desc_short = request.form.get("description_short")
+            desc_long = request.form.get("description_long")
+
+            file = request.files.get("profile_img")
+            # upload_path = os.path.join(
+            #     BASE_DIR, "domains", "stream", "static", "uploaded", "profiles"
+            # )
+            # os.makedirs(upload_path, exist_ok=True)
+            upload_path = os.path.join(stream_bp.static_folder, "uploaded_profiles")
+            os.makedirs(upload_path, exist_ok=True)
+            file_name = secure_filename(f"{id}_{file.filename}")
+            file.save(os.path.join(upload_path, file_name))
+
+            time_formatted = get_current_time_stamp_formated()
+
+            profiles[id] = {
+                "ID": id,
+                "NAME": name,
+                "AGE": age,
+                "SHORT_DESCRIPTION": desc_short,
+                "DESCRIPTION": desc_long,
+                "IMAGE": file_name,
+                "REG_DATE": time_formatted,
+                "MOD_DATE": time_formatted,
+            }
+
+        elif action == "delete":
+            id = request.form.get("id")
+            del profiles[id]
+
+        save_json(TARGETS_PROFILES_FILE, profiles)
+        return redirect(url_for("stream.profile"))
+
+    # 검색, 페이지네이션
     profiles = list(load_json(TARGETS_PROFILES_FILE).values())
+
+    kwd = request.args.get("search_keyword")
+    tag = request.args.get("search_tag")
+    profiles = filter_keyword(profiles, kwd, tag)
+
+    order = request.args.get("sort_order")
+    profiles = sort_accounts(profiles, order)
 
     per_page = int(request.args.get("per_page", 10))
     page = int(request.args.get("page", 1))
-
     profiles, total_pages = paginate(profiles, page, per_page)
 
     return render_template(
